17e347ddf4b48de721941d2c73deb2d2c0510a4e
Oloruntobhe <fataioloruntobi@gmail.com>
2022-04-08 16:16:53 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - execution starts.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char a = 'a';
+char b = 'A';
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+for (b = 'A'; b <= 'Z'; b++)
+{
+putchar(b);
+}
+putchar('\n');
+return (0);
+}
