05ff1bf2a7c69a9be5e47fc8b785baa212529470
Oloruntobhe <fataioloruntobi@gmail.com>
2022-04-14 19:58:33 -0700

    Update

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,28 +7,6 @@
  *Return: 0 always
  */
 
-
-
-int main(void)
-{
-long a, max_p;
-long num = 612852475143;
-double sqr = sqrt(num);
-
-for (a = 1; a <= sqr; a++)
-{
-if (num % a == 0)
-{
-max_p = num / a;
-}
-}
-
-printf("%ld\n", max_p);
-
-return (0);
-}
-
-
 int main(void)
 {
 long a, max_p;
