e1d79bc1fc75c8b6167a66976ca69040fb57b9f7
Oloruntobhe <fataioloruntobi@gmail.com>
2022-04-14 19:50:10 -0700

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ *main - Output the largest prime
+ *
+ *Return: 0 always
+ */
+
+
+
+int main(void)
+{
+long a, max_p;
+long num = 612852475143;
+double sqr = sqrt(num);
+
+for (a = 1; a <= sqr; a++)
+{
+if (num % a == 0)
+{
+max_p = num / a;
+}
+}
+
+printf("%ld\n", max_p);
+
+return (0);
+}
+
+
+int main(void)
+{
+long a, max_p;
+long num = 612852475143;
+double sqr = sqrt(num);
+
+for (a = 1; a <= sqr; a++)
+{
+if (num % a == 0)
+{
+max_p = num / a;
+}
+}
+
+printf("%ld\n", max_p);
+
+return (0);
+}
