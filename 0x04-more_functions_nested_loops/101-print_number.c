2f318b07db266c99158b3f89f9ffc4a0feb103cf
Oloruntobhe <fataioloruntobi@gmail.com>
2022-04-14 19:54:10 -0700

    Print number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_number - print an integer
+ *
+ *@n: integer to be printed
+ *
+ *Return: void
+ */
+
+void print_number(int n)
+{
+unsigned int num = n;
+
+if (n < 0)
+{
+n *= -1;
+num = n;
+_putchar('-');
+}
+
+num /= 10;
+
+if (num != 0)
+print_number(num);
+
+_putchar((unsigned int) n % 10 + '0');
+}
