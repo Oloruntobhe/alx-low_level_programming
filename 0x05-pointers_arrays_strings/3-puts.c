70366102852be5390288c34d751c1f3699a6ce48
Oloruntobhe <fataioloruntobi@gmail.com>
2022-04-21 09:13:13 -0700

    Put

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - main function.
+ * @str: Pointer with the address value.
+ *
+ * Return: none.
+ */
+
+
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
+}
